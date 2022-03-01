b85bd8394c7250a48eb07d5e2a00784293c6e32c
Jeeson Johnson <jeesonjohnson@outlook.com>
2022-03-01 22:53:38 +0000

    Initial setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,7 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <h1>Sheefo Rocks
-        
-      </h1>
+      <h1>Sheefo Rocks</h1>
     </div>
   )
 }
