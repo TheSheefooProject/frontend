3284240435d7de895e7a45a2bb49e20b3a80a37b
June <khazoda@gmail.com>
2022-03-02 01:41:03 +0000

    ~ devpage edit

--- pages/devpage/index.tsx
+++ pages/devpage/index.tsx
@@ -1,11 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
 
 const Devpage: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-primary/75 py-2">
-      <h1>Sheefoo Rocks</h1>
+    <div className="static min-h-screen bg-neutral px-[10vw] pt-12">
+      <Head>
+        <title>Sheefoo | Dev Page</title>
+      </Head>
+
+      <h1 className="border-b-2 border-secondary pb-2 text-4xl text-primary">
+        Sheefoo Dev Page
+      </h1>
+      <h2 className="py-2">
+        This is a development environment to collate styled components and
+        create a cohesive style.
+      </h2>
     </div>
   )
 }
