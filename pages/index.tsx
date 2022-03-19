e387a7c20586a2a876f9e065ed3fd34f8206f6d0
June <khazoda@gmail.com>
2022-03-19 19:59:24 +0000

    ~ modal style changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,13 +24,17 @@ const Home: NextPage = () => {
       <div className="relative mx-2 my-2 mt-12 flex w-[100%] flex-col rounded-md bg-back_3 px-2 md:mx-4 md:my-4">
         <div className="flex-1"></div>
 
-        {/* Modal shown when input is focused */}
+        {/* Modal (shown when input is focused) */}
         <div
           className={
             `${modal_showing ? ' visible' : 'hidden'}` +
-            ' absolute bottom-0 left-0 h-20 w-20 flex-1 bg-black'
+            ' absolute bottom-14 left-2 h-20 w-20 flex-1 rounded-md bg-black text-text_1'
           }
-        ></div>
+          style={{ width: 'calc(100% - 1rem)' }}
+        >
+          {' '}
+          MODAL
+        </div>
 
         {/* Text input box */}
         <span
