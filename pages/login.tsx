40920de9347d061a5d19a38f5080a3a8d8902f09
rdeoliveira527 <r.deoliveira527@gmail.com>
2022-03-22 16:23:28 +0000

    Login page

---
+++ pages/login.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import Button from '../components/common/Button'
+import Switch from '../components/common/Switch'
