c89020cd8b293fa31cae352d467f0bc06de36136
rdeoliveira527 <r.deoliveira527@gmail.com>
2022-03-22 16:31:17 +0000

    Test

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,3 +6,10 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Button from '../components/common/Button'
 import Switch from '../components/common/Switch'
+
+<div
+id="text_input_container"
+className={
+    ' relative bottom-0 max-h-[30vh] w-[100%]'
+}
+>text</div>
