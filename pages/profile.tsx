aa6322d47ac9688caa86673a352f85ceb86070ec
June <khazoda@gmail.com>
2022-03-24 19:29:31 +0000

    ~ Profile page style change

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -5,7 +5,11 @@ import { useState } from 'react'
 import TextBox from '../components/common/TextBox'
 
 const Profile: NextPage = () => {
-  return <div>Profile</div>
+  return (
+    <main className="md:[5vw] static min-h-screen w-[100%] min-w-[320px] overflow-x-hidden bg-back_3 px-10 pt-12 md:px-[calc(20vw+5rem)]">
+      Profile
+    </main>
+  )
 }
 
 export default Profile
