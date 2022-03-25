0f1f8f2087c3b967dca6be47fe119310dd2aaf3d
June <khazoda@gmail.com>
2022-03-25 12:26:50 +0000

    + Profile account change buttons added

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -3,12 +3,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect } from 'react'
 import { useState } from 'react'
+import Button from '../components/common/Button'
 import TextArea from '../components/common/TextArea'
 import TextBox from '../components/common/TextBox'
 
 const Profile: NextPage = () => {
   return (
-    <main className="md:[5vw] static min-h-screen w-[100%] min-w-[320px] overflow-x-hidden bg-back_3 px-10 pt-12 text-text_1 md:px-[calc(20vw+5rem)]">
+    <main className="md:[5vw] static flex min-h-screen w-[100%] min-w-[320px] flex-col justify-between overflow-x-hidden bg-back_3 px-10 pt-12 text-text_1 md:px-[calc(20vw+5rem)]">
+      {/* Profile Container */}
       <div className="flex flex-col font-body md:flex-row">
         <div
           id="userimage"
@@ -33,6 +35,16 @@ const Profile: NextPage = () => {
               </li>
             </ul>
           </form>
+        </div>
+      </div>
+      {/* Change User Settings Container */}
+      <div className="mb-10 flex flex-col">
+        <div className="flex flex-col md:flex-row">
+          <Button text="Change E-Mail" type="neutral"></Button>
+          <Button text="Change Password" type="neutral"></Button>
+        </div>
+        <div className="flex flex-col md:flex-row">
+          <Button text="Permanently Delete Account" type="negative"></Button>
         </div>
       </div>
     </main>
