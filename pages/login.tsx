56e09b45ce6edb51ee46a9f11e838245fce6b9bc
rdeoliveira527 <r.deoliveira527@gmail.com>
2022-03-29 13:19:22 +0100

    Basic login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,47 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import Link from 'next/link'
 import Image from 'next/image'
 import Button from '../components/common/Button'
-import Switch from '../components/common/Switch'
-import TextBox from '../components/common/TextBox'
 
 const Login: NextPage = () => {
   return (
-    <div
-      id="text_input_container"
-      className={' relative bottom-0 max-h-[30vh] w-[100%]'}
-    >
-      <TextBox className="m-2"></TextBox>
-    </div>
+    <main className="static flex min-h-screen w-[100%] min-w-[320px] flex-col justify-between overflow-x-hidden bg-back_3 px-10 pt-12 text-text_1  md:px-[calc(25vw+5rem)]">
+      {/* Profile Container */}
+      <div className="flex flex-col justify-between self-center font-body md:flex-row">
+        <div id="loginlogo" className="mr-10 h-48 w-48 overflow-hidden">
+          <Image src="/logo.svg" width={500} height={500}></Image>
+        </div>
+        <div className="self-center rounded border-0 bg-back_1 p-6">
+          <ul>
+            <li className="flex w-96 flex-col gap-1">
+              <label>Username </label>
+              <input
+                className="rounded bg-back_4"
+                type="text"
+                name="username"
+                required
+              />
+            </li>
+            <li className="mt-3 flex flex-col gap-1">
+              <label>Password </label>
+              <input
+                className="rounded bg-back_4"
+                type="password"
+                name="password"
+                required
+              />
+            </li>
+            <li className="mt-4 flex flex-col gap-1">
+              <Button
+                noMargin
+                text="Login"
+                type="neutral"
+                className="mb-5 md:mb-0"
+              ></Button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
   )
 }
 
