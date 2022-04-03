0672e354c252550a778f685db2db8f7c2cd2a1fc
June <khazoda@gmail.com>
2022-04-04 00:45:18 +0100

    +app-wide settings state, switch initials

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,9 +10,68 @@ import Button from '../components/common/Button'
 let body: HTMLBodyElement | null = null
 let localStorage: Storage
 
+export type ISettings = {
+  text_size: number
+  dark: boolean
+  reduced_motion: boolean
+  disable_autoplay: boolean
+  disable_sounds: boolean
+  profile_private: boolean
+  dms_from_strangers: boolean
+  disable_notifications: boolean
+}
+const defaultSettings = {
+  text_size: 5,
+  dark: false,
+  reduced_motion: false,
+  disable_autoplay: false,
+  disable_sounds: false,
+  profile_private: false,
+  dms_from_strangers: false,
+  disable_notifications: false,
+}
 function MyApp({ Component, pageProps }: AppProps) {
   const [is_dark, setIsDark] = useState<boolean>()
+  const [settings, setSettings] = useState<ISettings>()
 
+  const saveSettings = (key: string, val: number | boolean) => {
+    if (localStorage !== undefined) {
+      switch (key) {
+        case 'text_size':
+          break
+        case 'dark':
+          break
+        case 'reduced_motion':
+          break
+        case 'disable_autoplay':
+          break
+        case 'disable_sounds':
+          break
+        case 'profile_private':
+          break
+        case 'dms_from_strangers':
+          break
+        case 'disable_notifications':
+          break
+
+        default:
+          break
+      }
+      let tempCachedSettings = JSON.parse(localStorage.settings)
+      tempCachedSettings[key] = val
+      localStorage.settings = JSON.stringify(tempCachedSettings)
+      // localStorage.settings = JSON.stringify({
+      //   text_size: text_size,
+      //   dark: dark,
+      //   reduced_motion: reduced_motion,
+      //   disable_autoplay: disable_autoplay,
+      //   disable_sounds: disable_sounds,
+      //   profile_private: profile_private,
+      //   dms_from_strangers: dms_from_strangers,
+      //   disable_notifications: disable_notifications,
+      // })
+    }
+  }
   // Light/Dark theme switching function
   const setDark = (val: boolean) => {
     if (body != null) {
@@ -39,6 +98,13 @@ function MyApp({ Component, pageProps }: AppProps) {
     body = document.querySelector('body')
     localStorage = window.localStorage
 
+    console.log(localStorage.settings)
+
+    if (localStorage.settings !== undefined) {
+      setSettings(JSON.parse(localStorage.settings))
+    } else {
+      localStorage.settings = JSON.stringify(defaultSettings)
+    }
     // Initially set theme to light
     if (body != null) {
       body.classList.add('theme-light')
@@ -54,7 +120,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <div className="flex flex-row-reverse">
-      <Component is_dark={is_dark} setDark={setDark} {...pageProps} />
+      <Component
+        is_dark={is_dark}
+        setDark={setDark}
+        settings={settings}
+        saveSettings={saveSettings}
+        {...pageProps}
+      />
       <Sidebar></Sidebar>
     </div>
   )
--- pages/settings.tsx
+++ pages/settings.tsx
@@ -6,13 +6,24 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Button from '../components/common/Button'
 import Switch from '../components/common/Switch'
-
+import { ISettings } from './_app'
 let body: HTMLBodyElement | null = null
 let localStorage: Storage
 
-const SettingsPage = (props: { is_dark: boolean; setDark: Function }) => {
+const SettingsPage = (props: {
+  is_dark: boolean
+  setDark: Function
+  settings: ISettings
+  saveSettings: Function
+}) => {
   // Default prop values
-  const { is_dark = false, setDark = null, ...restProps } = props
+  const {
+    is_dark = false,
+    setDark = null,
+    settings = null,
+    saveSettings = null,
+    ...restProps
+  } = props
 
   return (
     <main className="md:[5vw] static min-h-screen w-[100%] min-w-[320px] overflow-x-hidden bg-back_3 px-10 pt-12 md:px-[calc(20vw+5rem)]">
@@ -64,7 +75,15 @@ const SettingsPage = (props: { is_dark: boolean; setDark: Function }) => {
         {/* Reduced Motion Toggle */}
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-xl ">Reduced motion</h2>
-          <Switch name="setting_motion"></Switch>
+          <Switch
+            name="setting_motion"
+            onClick={(e: { target: { checked: boolean } }) => (
+              console.log(settings),
+              console.log(typeof settings),
+              props.saveSettings('reduced_motion', e.target.checked)
+            )}
+            initialState={settings?.reduced_motion}
+          ></Switch>
         </div>
 
         {/* Divider */}
@@ -72,7 +91,14 @@ const SettingsPage = (props: { is_dark: boolean; setDark: Function }) => {
         {/* Disable autoplay toggle */}
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-xl ">Disable video/GIF autoplay</h2>
-          <Switch name="setting_autoplay"></Switch>
+          <Switch
+            name="setting_autoplay"
+            onClick={(e: { target: { checked: boolean } }) => (
+              console.log(settings),
+              props.saveSettings('disable_autoplay', e.target.checked)
+            )}
+            initialState={settings?.disable_autoplay}
+          ></Switch>
         </div>
 
         {/* Divider */}
@@ -80,7 +106,14 @@ const SettingsPage = (props: { is_dark: boolean; setDark: Function }) => {
         {/* Disable Sound toggle */}
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-xl ">Disable sounds</h2>
-          <Switch name="setting_sound"></Switch>
+          <Switch
+            name="setting_sound"
+            onClick={(e: { target: { checked: boolean } }) => (
+              console.log(settings),
+              props.saveSettings('disable_sounds', e.target.checked)
+            )}
+            initialState={settings?.disable_sounds}
+          ></Switch>
         </div>
 
         <h2 className=" mt-10 border-b-[1px] border-back_2 pb-2 text-left font-heading text-2xl text-text_2">
@@ -91,32 +124,53 @@ const SettingsPage = (props: { is_dark: boolean; setDark: Function }) => {
         {/* Reduced Motion Toggle */}
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-xl ">Profile visibility</h2>
-          <Switch name="setting_visibility"></Switch>
+          <Switch
+            name="setting_visibility"
+            onClick={(e: { target: { checked: boolean } }) => (
+              console.log(settings),
+              props.saveSettings('profile_private', e.target.checked)
+            )}
+            initialState={settings?.profile_private}
+          ></Switch>
         </div>
         {/* Divider */}
         <span className="relative bottom-0 my-4 h-[1px] w-auto bg-back_4 opacity-20 "></span>
         {/* Reduced Motion Toggle */}
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-xl ">Receive direct messages from</h2>
-          <Switch name="setting_dms"></Switch>
+          <Switch
+            name="setting_dms"
+            onClick={(e: { target: { checked: boolean } }) => (
+              console.log(settings),
+              props.saveSettings('dms_from_strangers', e.target.checked)
+            )}
+            initialState={settings?.dms_from_strangers}
+          ></Switch>
         </div>
         {/* Divider */}
         <span className="relative bottom-0 my-4 h-[1px] w-auto bg-back_4 opacity-20 "></span>
         {/* Reduced Motion Toggle */}
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-xl ">Disable push notifications</h2>
-          <Switch name="setting_notifications"></Switch>
+          <Switch
+            name="setting_notifications"
+            onClick={(e: { target: { checked: boolean } }) => (
+              console.log(settings),
+              props.saveSettings('disable_notifications', e.target.checked)
+            )}
+            initialState={settings?.disable_notifications}
+          ></Switch>
         </div>
 
         {/* Divider */}
         <span className="relative bottom-0 my-4 h-[1px] w-auto bg-accent_1 opacity-40 "></span>
         {/* Delete Account Button */}
         <div className="flex flex-row items-center justify-between">
-          <h2 className="text-xl ">Permanently Delete Account</h2>
+          <h2 className="text-xl ">Restore default settings</h2>
           <Button
-            text="DELETE"
+            text="DEFAULTS"
             type="negative"
-            onClick={() => alert('☠️ Account Deleted ☠️')}
+            onClick={() => alert('☠️ Default Settings Restored ☠️')}
             noMargin
             fixedWidth
           ></Button>
